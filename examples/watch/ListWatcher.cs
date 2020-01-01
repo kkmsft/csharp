b558a56f0d34bfa2ee8710891059edc3e15b523d
Krishnakumar R(KK) <29471693+kkmsft@users.noreply.github.com>
2019-12-31 16:01:38 -0800

    Intermediate checkin - introduce listwatcher

--- examples/watch/ListWatcher.cs
+++ examples/watch/ListWatcher.cs
@@ -8,133 +8,34 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using k8s;
 using k8s.Models;
+using System;
 
-namespace watch
+namespace informers
 {
-    public class ListWatcher : ServiceClient<Kubernetes>
+    public delegate Task<HttpOperationResponse<T>> ListerD<T>(string namespaceParameter, string apiVersionParameter, string kindParameter, CancellationToken cancellationToken, bool watch);
+    public delegate Watcher<T> WatcherD<T, L>(Action<WatchEventType, T> onEvent, Action<Exception> onError, Action onClosed);
+
+    public class ListWatcher<T, L>
     {
-        public JsonSerializerSettings SerializationSettings { get; private set; }
+        
+        //public delegate Task ListerD(IKubernetes c, string ns, string version, string kind, CancellationToken t, bool watch);
 
-        /// <summary>
-        /// Gets or sets json deserialization settings.
-        /// </summary>
-        public JsonSerializerSettings DeserializationSettings { get; private set; }
-
-        public void InitSerialization() {
-
-                SerializationSettings = new JsonSerializerSettings
-                    {
-                        Formatting = Newtonsoft.Json.Formatting.Indented,
-                        DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat,
-                        DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc,
-                        NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
-                        ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize,
-                        ContractResolver = new ReadOnlyJsonContractResolver(),
-                        Converters = new  List<JsonConverter>
-                            {
-                                new Iso8601TimeSpanConverter()
-                            }
+        public ListerD<T> Lister;
+        public WatcherD<T,L> Watcher;
+        string _apiVersion;
+        string _nameSpace;
+        string _kindParameter;
+        CancellationToken _cancellationToken;   
+        
+        public ListWatcher(IKubernetes client) {                  
+            Lister = delegate(string nameSpace, string apiVersion, string kindParameter, CancellationToken cancellationToken, bool watch) 
+                    { 
+                        return client.List<T>(_nameSpace, _apiVersion, _kindParameter, _cancellationToken, false);
                     };
-                    DeserializationSettings = new JsonSerializerSettings
+            Watcher = delegate(Action<WatchEventType, T> onEvent, Action<Exception> onError, Action onClosed)
                     {
-                        DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat,
-                        DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc,
-                        NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
-                        ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize,
-                        ContractResolver = new ReadOnlyJsonContractResolver(),
-                        Converters = new List<JsonConverter>
-                            {
-                                new Iso8601TimeSpanConverter()
-                            }
+                        return client.List<T>("default", "v1", "pods", watch:true).Watch(onEvent, onError, onClosed);                         
                     };
         }
-
-
-        
-        public async Task<HttpOperationResponse<T>> List<T>(string namespaceParameter, string apiVersionParameter, string kindParameter, CancellationToken cancellationToken=default(CancellationToken), bool? watch = default(bool?)) {                                    
-            // Construct URL                       
-            var _url = new System.Uri("http://localhost:8080/api/{apiVersion}/namespaces/{namespace}/{kind}").ToString();
-            _url = _url.Replace("{namespace}", System.Uri.EscapeDataString(namespaceParameter));
-            _url = _url.Replace("{apiVersion}", System.Uri.EscapeDataString(apiVersionParameter));
-            _url = _url.Replace("{kind}", System.Uri.EscapeDataString(kindParameter));
-            
-            List<string> _queryParameters = new List<string>();
-            if (watch != null)
-            {
-                _queryParameters.Add(string.Format("watch={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(watch, SerializationSettings).Trim('"'))));
-            }          
-            
-            if (_queryParameters.Count > 0)
-            {
-                _url += "?" + string.Join("&", _queryParameters);
-            }
-
-
-            HttpResponseMessage _httpResponse = null;
-            string _requestContent = null;
-
-
-            // Create HTTP transport objects
-            var _httpRequest = new HttpRequestMessage();            
-            _httpRequest.Method = new HttpMethod("GET");
-            _httpRequest.RequestUri = new System.Uri(_url);            
-        
-
-            cancellationToken.ThrowIfCancellationRequested();          
-            System.Console.WriteLine("Here about to call GET: {0}", _httpRequest.RequestUri.ToString());  
- 
-            _httpResponse = await HttpClient.SendAsync(_httpRequest, cancellationToken).ConfigureAwait(false);  
-
-            HttpStatusCode _statusCode = _httpResponse.StatusCode;
-            System.Console.WriteLine("Here later");  
-            cancellationToken.ThrowIfCancellationRequested();
-
-            string _responseContent = null;
-
-            if ((int)_statusCode != 200 && (int)_statusCode != 401)
-            {
-                var ex = new HttpOperationException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
-                if (_httpResponse.Content != null) {
-                    _responseContent = await _httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
-                }
-                else {
-                    _responseContent = string.Empty;
-                }
-                ex.Request = new HttpRequestMessageWrapper(_httpRequest, _requestContent);
-                ex.Response = new HttpResponseMessageWrapper(_httpResponse, _responseContent);              
-                _httpRequest.Dispose();
-                if (_httpResponse != null)
-                {
-                    _httpResponse.Dispose();
-                }
-                throw ex;
-            }
-
-          
-            // Create Result
-            var _result = new HttpOperationResponse<T>();
-            _result.Request = _httpRequest;
-            _result.Response = _httpResponse;
-
-            // Deserialize Response
-            if ((int)_statusCode == 200)
-            {
-                _responseContent = await _httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
-                try
-                {
-                    _result.Body = SafeJsonConvert.DeserializeObject<T>(_responseContent, DeserializationSettings);
-                }
-                catch (JsonException ex)
-                {
-                    _httpRequest.Dispose();
-                    if (_httpResponse != null)
-                    {
-                        _httpResponse.Dispose();
-                    }
-                    throw new SerializationException("Unable to deserialize the response.", _responseContent, ex);
-                }
-            }            
-            return _result; 
-        }
-    }
+    }        
 }
--- examples/watch/Program.cs
+++ examples/watch/Program.cs
@@ -11,22 +11,23 @@ namespace watch
        
         public static void Main(string[] args)
         {
-            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(Environment.GetEnvironmentVariable("KUBECONFIG"));
 
             IKubernetes client = new Kubernetes(config);
+            Console.WriteLine("Creds: {0}", client.Credentials);
 
-          /*  ListWatcher lw = new ListWatcher();
+           /* ListWatcher lw = new ListWatcher();
             lw.InitSerialization();
-            var response = lw.List<V1PodList>("default", "v1", "pods").Result;            
+            var response = lw.List<V1PodList>(client, "default", "v1", "pods").Result;            */
+
+            var response = client.List<V1PodList>("default", "v1", "pods").Result;
             Console.WriteLine("{0}", response.Body.Items.Count);
             for (var i=0; i<response.Body.Items.Count; i++) {
                 Console.WriteLine("{0}", response.Body.Items[i].Metadata.Name);
             }
-           */ 
-            //var w = lw.List<V1PodList>("default", "v1", "pods", watch:true); 
-            
-            var podlistResp = client.ListNamespacedPodWithHttpMessagesAsync("default", watch: true);
-            using (podlistResp.Watch<V1Pod, V1PodList>((type, item) =>
+            Console.WriteLine("Watch");
+            var w = client.List<V1PodList>("default", "v1", "pods", watch:true); 
+            using (w.Watch<V1Pod, V1PodList>((type, item) =>
             {
                 Console.WriteLine("==on watch event==");
                 Console.WriteLine(type);
