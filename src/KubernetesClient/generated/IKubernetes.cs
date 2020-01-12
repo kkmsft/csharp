22884911bc2dc73cefccba4f12a641a4f1c8876a
Krishnakumar R(KK) <29471693+kkmsft@users.noreply.github.com>
2020-01-12 12:41:04 -0800

    Reset some files to master

--- src/KubernetesClient/generated/IKubernetes.cs
+++ src/KubernetesClient/generated/IKubernetes.cs
@@ -18,7 +18,28 @@ namespace k8s
     /// <summary>
     /// </summary>
     public partial interface IKubernetes : System.IDisposable
-    {        
+    {
+        /// <summary>
+        /// The base URI of the service.
+        /// </summary>
+        System.Uri BaseUri { get; set; }
+
+        /// <summary>
+        /// Gets or sets json serialization settings.
+        /// </summary>
+        JsonSerializerSettings SerializationSettings { get; }
+
+        /// <summary>
+        /// Gets or sets json deserialization settings.
+        /// </summary>
+        JsonSerializerSettings DeserializationSettings { get; }
+
+        /// <summary>
+        /// Subscription credentials which uniquely identify client
+        /// subscription.
+        /// </summary>
+        ServiceClientCredentials Credentials { get; }
+
 
         /// <summary>
         /// get available API versions
