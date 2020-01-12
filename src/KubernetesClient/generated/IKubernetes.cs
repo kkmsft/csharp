8a2d9ede037cabf897d2b7b30733f26ddfb90aa7
Krishnakumar R(KK) <29471693+kkmsft@users.noreply.github.com>
2020-01-12 12:22:27 -0800

    Move List out of generated code

--- src/KubernetesClient/generated/IKubernetes.cs
+++ src/KubernetesClient/generated/IKubernetes.cs
@@ -18,29 +18,7 @@ namespace k8s
     /// <summary>
     /// </summary>
     public partial interface IKubernetes : System.IDisposable
-    {
-        /// <summary>
-        /// The base URI of the service.
-        /// </summary>
-        System.Uri BaseUri { get; set; }
-
-        /// <summary>
-        /// Gets or sets json serialization settings.
-        /// </summary>
-        JsonSerializerSettings SerializationSettings { get; }
-
-        /// <summary>
-        /// Gets or sets json deserialization settings.
-        /// </summary>
-        JsonSerializerSettings DeserializationSettings { get; }
-
-        /// <summary>
-        /// Subscription credentials which uniquely identify client
-        /// subscription.
-        /// </summary>
-        ServiceClientCredentials Credentials { get; }
-
-        Task<HttpOperationResponse<L>> List<T, L>(string namespaceParameter, CancellationToken cancellationToken=default(CancellationToken), bool? watch = default(bool?));
+    {        
 
         /// <summary>
         /// get available API versions
